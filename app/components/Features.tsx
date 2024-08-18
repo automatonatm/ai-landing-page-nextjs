15d94a1049f45f26e13b9c7b5072e06ba228306f
Automaton <46001539+automatonatm@users.noreply.github.com>
2024-08-18 21:09:04 +0100

    Mak added

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -1,6 +1,7 @@
+"use client";
 import Image from "next/image";
-import { title } from "process";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 
 const features = [
   {
@@ -22,6 +23,59 @@ const features = [
   },
 ];
 
+const Feature = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  const offsetX = useMotionValue(-100);
+  const offsetY = useMotionValue(-100);
+
+  const maskImage = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px, black, transparent)`;
+  const border = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const updateMousePosition = (e: MouseEvent) => {
+      // e.x
+
+      // e.y
+
+      if (!border.current) return;
+      const borderRectangle = border.current?.getBoundingClientRect();
+      offsetX.set(e.x - borderRectangle?.x);
+      offsetY.set(e.y - borderRectangle?.y);
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return (
+    <div className="relative flex max-w-[360px] flex-col items-center justify-center gap-6 rounded-xl border border-white/30 bg-[#0D0D0D] p-10">
+      <motion.div
+        className="absolute inset-0 rounded-xl border-2 border-purple-400"
+        style={{ maskImage, WebkitMaskImage: maskImage }}
+        ref={border}
+      ></motion.div>
+      <div>
+        <Image
+          src="/assets/icons/ecosystem.svg"
+          alt="ecosystem"
+          width={56}
+          height={56}
+        />
+      </div>
+      <div className="space-y-2 text-center">
+        <h3 className="text-base font-bold text-white">{title}</h3>
+        <p className="font-inter text-white">{description}</p>
+      </div>
+    </div>
+  );
+};
+
 const Features = () => {
   return (
     <div className="bg-black py-[72px]">
@@ -37,28 +91,12 @@ const Features = () => {
 
         <div className="mt-[64px] flex flex-col items-center justify-center gap-4 lg:flex-row">
           {features.map(({ title, description }) => (
-            <div
-              key={title}
-              className="flex max-w-[360px] flex-col items-center justify-center gap-6 rounded-lg border border-white/30 bg-[#0D0D0D] p-10"
-            >
-              <div>
-                <Image
-                  src="/assets/icons/ecosystem.svg"
-                  alt="ecosystem"
-                  width={56}
-                  height={56}
-                />
-              </div>
-              <div className="space-y-2 text-center">
-                <h3 className="text-base font-bold text-white">{title}</h3>
-                <p className="font-inter text-white">{description}</p>
-              </div>
-            </div>
+            <Feature key={title} title={title} description={description} />
           ))}
         </div>
       </div>
     </div>
   );
 };
- 
+
 export default Features;
